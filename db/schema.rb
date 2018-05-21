5b9df737a2e00826a96352eeeb50e1618aa6b282
projectormato <k.keita811.anuy@gmail.com>
2018-05-22 01:46:17 +0900

    create some user test.(include secure passwords)

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_21_155517) do
+ActiveRecord::Schema.define(version: 2018_05_21_163914) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
