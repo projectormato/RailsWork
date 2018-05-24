ffeb4c051632d72a17ed9dc46be4800821390bb6
projectormato <k.keita811.anuy@gmail.com>
2018-05-24 20:30:45 +0900

    create login system.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,16 @@
 module SessionsHelper
+  # 引数のユーザでログインする
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  #現在のユーザーを返す
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  # ユーザーがログインしていればtrue、その他ならfalseを返す
+  def logged_in?
+    !current_user.nil?
+  end
 end
